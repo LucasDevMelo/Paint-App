8fb6142b1c6d9bbf2ab064fa5e324ac8838872d5
Lucas Lopes <lucaslopesdemelosilva@gmail.com>
2024-02-05 15:47:56 -0300

    update 5-2-2024

--- app/src/main/java/com/example/paintapp/MainActivity.kt
+++ app/src/main/java/com/example/paintapp/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.paintapp
 
+import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.Toast
+import com.example.paintapp.PaintView.Companion.colorList
+import com.example.paintapp.PaintView.Companion.currentBrush
+import com.example.paintapp.PaintView.Companion.pathList
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,19 +29,33 @@ class MainActivity : AppCompatActivity() {
 
         redBtn.setOnClickListener {
             Toast.makeText(this,"Clicked", Toast.LENGTH_SHORT).show()
+            paintBrush.color = Color.RED
+            currentColor(paintBrush.color)
         }
 
         blueBtn.setOnClickListener {
             Toast.makeText(this,"Clicked", Toast.LENGTH_SHORT).show()
+            paintBrush.color = Color.BLUE
+            currentColor(paintBrush.color)
         }
 
         blackBtn.setOnClickListener {
             Toast.makeText(this,"Clicked", Toast.LENGTH_SHORT).show()
+            paintBrush.color = Color.BLACK
+            currentColor(paintBrush.color)
         }
 
         eraser.setOnClickListener {
             Toast.makeText(this,"Clicked", Toast.LENGTH_SHORT).show()
+            pathList.clear()
+            colorList.clear()
+            path.reset()
         }
 
+    }
+
+    private fun currentColor(color: Int){
+        currentBrush = color
+        path = Path()
     }
 }
